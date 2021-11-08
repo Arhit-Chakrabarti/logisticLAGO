19c9b52606289844a0333d826d4f286836643110
Arhit Chakrabarti <arhit.chakrabarti@tamu.edu>
2021-11-07 19:31:20 -0600

    Added the jitter_my and truncate functions needed for the lago simulation codes

--- R/lago_aux_functions.R
+++ R/lago_aux_functions.R
@@ -28,3 +28,21 @@ exp_my <- function(beta, x, intercept = TRUE){
   linear_comp <- sum(beta * u) # Calculate the value of the linear predictor \beta^T X
   return(exp(linear_comp)) # Return the value of my exp function
 }
+## Defining the function to jitter observations for the simulation codes
+jitter_my <- function(x, n, variation){# Takes as input the vector of values x, sample size n and the variation parameter which controls the amount of jittering induced by the function about the initial x values
+  x_jitter = x + (variation * x * t(matrix(stats::rnorm(n * length(x), mean = 0, sd = 1), nrow = n)))
+  return(t(x_jitter))
+}
+## Defining the truncate function to truncate the actual interventions after jittering the starting/optimum intervention at any stage
+truncate <- function(x, lower.limit, upper.limit){
+  # If any value x is below the lower range for the corresponding component, replace the value with the lower limit
+  first_part = lower.limit * Indicator(t(x) < lower.limit)
+  # If the value of x lies between the lower and upper limits then return the corresponding value
+  second_part = t(x) * Indicator(lower.limit < t(x)) * Indicator(t(x) < upper.limit)
+  # If any value x is above the upper range for the corresponding component, replace the value with the upper limit
+  third_part = upper.limit * Indicator(t(x) > upper.limit)
+  truncated = first_part + second_part + third_part # Adding the three cases together
+  return(t(truncated)) # Returning the vector of truncated values
+}
+
+
