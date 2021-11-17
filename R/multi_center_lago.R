c089a6531711ba1fce481705d490907b452953b6
Arhit Chakrabarti <arhit.chakrabarti@tamu.edu>
2021-11-17 00:15:41 -0600

    Added description to the function mc_lago and changed some format of the function documentation

--- R/multi_center_lago.R
+++ R/multi_center_lago.R
@@ -1,5 +1,5 @@
 #' Optimum interventions under the multi center LAGO design
-#'
+#' @description This function takes as input several study design parameters of a multi-stage, multi-center per stage LAGO design and provides the estimated optimal intervention package through simulations. For simplicity, this function assumes an equal number of centers per stage and equal number of samples per center in each stage. The required parameters that need to be supplied to the function are an initial starting intervention package which is optimized over the stages, the lower and upper limits for the components of the intervention package, the true or best guess beta values, number of stages K, number of centers per stage J, sample size per center per stage n, the unit costs for the intervention package components and the desired outcome goal for the LAGO study. Since this function returns the estimated optimal interventions through simulations, this function also needs as input the expected variation among the subjects within and between the centers at any stage.
 #' @param x0 a vector of starting values for the components of the intervention package
 #' @param lower a vector providing the values of lower limits for the components of the intervention package
 #' @param upper a vector providing the values of upper limits for the components of the intervention package
@@ -7,7 +7,7 @@
 #' @param centers the number of centers per stage of the LAGO design
 #' @param beta.true a vector of true i.e. population beta values
 #' @param sample.size sample size (n) in each center of each of the K stages
-#' @param icc the expected variation among subjects within and between the center at any stage
+#' @param icc the expected variation among subjects within and between the centers at any stage
 #' @param cost.vec a vector of per unit linear cost coefficients
 #' @param prob the desired outcome goal
 #' @param intercept a logical argument to include intercept in the model or not (default = TRUE)
@@ -26,9 +26,14 @@
 #' njk = 3 # Number of centers per stage
 #' cost_lin = c(1, 8, 2.5) # Costs
 #' p_bar = 0.9 # Desired outcome goal
-#' beta = c(log(0.05), log(1.2), log(1.1), log(1.3)) # True beta values
+#' # True/best guess beta values
+#' beta = c(log(0.05), log(1.2), log(1.1), log(1.3))
 #'
-#' sim_mc <- mc_lago(x0 = x.init, lower = x.l, upper = x.u, beta.true = beta, nstages = K, centers = J, sample.size = njk, icc = 0.1, prob = p_bar, cost.vec = cost_lin)
+#' sim_mc <- mc_lago(x0 = x.init, lower = x.l, upper = x.u,
+#'                   beta.true = beta, nstages = K,
+#'                   centers = J, sample.size = njk,
+#'                   icc = 0.1, prob = p_bar,
+#'                   cost.vec = cost_lin)
 #'
 mc_lago <- function(x0, lower, upper, nstages, centers, beta.true, sample.size, icc, cost.vec, prob, intercept = TRUE, B = 100){
   # Initializing objects for use in the simulation
