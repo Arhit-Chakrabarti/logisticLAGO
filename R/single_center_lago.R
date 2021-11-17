4ad8174150192700a7ee0f58b9773bc26c009fd2
Arhit Chakrabarti <arhit.chakrabarti@tamu.edu>
2021-11-17 00:14:41 -0600

    Added description to the function sc_lago and changed some format of the function documentation

--- R/single_center_lago.R
+++ R/single_center_lago.R
@@ -1,5 +1,5 @@
 #' Optimum interventions under the single center LAGO design
-#'
+#' @description This function takes as input several study design parameters of a multi-stage, single center per stage LAGO design and provides the estimated optimal intervention package through simulations. The required parameters that need to be supplied to the function are an initial starting intervention package which is optimized over the stages, the lower and upper limits for the components of the intervention package, the true or best guess beta values, number of stages K, sample size per stage n, the unit costs for the intervention package components and the desired outcome goal for the LAGO study. Since this function returns the estimated optimal interventions through simulations, this function also needs as input the expected variation among the subjects within the center at any stage.
 #' @param x0 a vector of starting values for the components of the intervention package
 #' @param lower a vector providing the values of lower limits for the components of the intervention package
 #' @param upper a vector providing the values of upper limits for the components of the intervention package
@@ -23,9 +23,14 @@
 #' K = 3 # Number of stages
 #' cost_lin = c(1, 8, 2.5) # Costs
 #' p_bar = 0.9 # Desired outcome goal
-#' beta = c(log(0.05), log(1.2), log(1.1), log(1.3)) # True beta values
+#' # True/best guess beta values
+#' beta = c(log(0.05), log(1.2), log(1.1), log(1.3))
 #'
-#' sim_sc <- sc_lago(x0 = x.init, lower = x.l, upper = x.u, nstages = K, beta.true = beta, sample.size = n, icc = 0.1, cost.vec = cost_lin, prob = p_bar, B = 100, intercept = TRUE)
+#' sim_sc <- sc_lago(x0 = x.init, lower = x.l, upper = x.u,
+#'                   nstages = K, beta.true = beta,
+#'                   sample.size = n, icc = 0.1,
+#'                   cost.vec = cost_lin, prob = p_bar,
+#'                   B = 100, intercept = TRUE)
 #'
 sc_lago <- function(x0, lower, upper, nstages, beta.true, sample.size, icc, cost.vec, prob, intercept = TRUE, B = 100){
   # Initializing objects for use in the simulation
